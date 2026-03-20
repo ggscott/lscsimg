ed8d840ea3dd17d638605ef915611a9f047b331e
Garrett Scott <ggscott@me.com>
2026-03-20 07:21:28 -0700

    rem files

--- patch_remove_static.py
+++
@@ -1,9 +0,0 @@
-with open('app.py', 'r') as f:
-    content = f.read()
-
-content = content.replace('app.mount("/static", StaticFiles(directory="static"), name="static")', '# Removed static mount')
-content = content.replace('os.makedirs("static", exist_ok=True)', '# Removed static directory creation')
-content = content.replace('from fastapi.staticfiles import StaticFiles', '# from fastapi.staticfiles import StaticFiles')
-
-with open('app.py', 'w') as f:
-    f.write(content)
